b7fad8a8e1323eec83b7d8a8aa3c911366723032
Diego Berdugo <104031707+BERDUGO93@users.noreply.github.com>
2022-05-21 14:20:18 -0500

    update
    update

---
+++ Ejercicio.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miscelanea_Diego_Berdugo
+{
+    class Ejercicios
+    {
+
+        public double AreaTriangulo(double numero1, double numero2)
+
+        {
+
+            Console.WriteLine("Escriba numeros enteros: ");
+
+            Console.WriteLine("Ingresar la base del triangulo: ");
+
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("los valores que esta ingresando no son numericos");
+
+            Console.WriteLine("Ingresar la altura del triangulo: ");
+
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("los valores que esta ingresando no son numericos");
+
+            Console.WriteLine("El area del triangulo es: " + (numero1 * numero2) / 2);
+            return 0.0;
+        }
+
+
+
+        public double SumaNumero(double numero1, double numero2)
+
+        {
+            Console.WriteLine("Escriba numeros enteros: ");
+            Console.WriteLine("Ingrese el primer entero: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo entero: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("La suma es: " + (numero1 + numero2));
+            return 0.0;
+
+        }
+
+        public double NumeroCuadrado(double numero1, double numero2)
+        {
+
+            Console.WriteLine("Escriba numeros enteros: ");
+            Console.WriteLine("Ingrese el numero que quiere elevar al cuadrado: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("el numero " + numero1 + " elevado al cuadrado es: " + (numero1 * 2));
+
+            return 0.0;
+        }
+
+        public double ConversorEuros(double numero1, double numero2)
+        {
+            Console.WriteLine("Ingrese la cantidad en Euros que desea convertir a dolares: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(numero1 + " Euros equivalen a: " + (numero1 * 1.06) + " dolares ");
+
+
+            return 0.0;
+        }
+
+        public double AreaPerimetro(double numero1)
+
+        {
+
+            Console.WriteLine("Escriba numeros enteros: ");
+            Console.WriteLine("Ingresar el lado del cuadrado: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("El area del cuadrado es: " + (numero1 * numero1) + " cm2");
+            Console.WriteLine("El perimetro del cuadrado es: " + (numero1 * 4));
+            return 0.0;
+        }
+        public double AreaVolumen(double numero1, double numero2)
+        {
+            Console.WriteLine("Escriba numeros enteros: ");
+            Console.WriteLine("Ingresar el radio del cilindro: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar la altura del cilindro: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("El area del cilindro es: " + (2 * Math.PI * numero1 * numero2));
+            Console.WriteLine("El Volumen del cilindro es: " + (Math.PI * numero1 * numero1 * numero2));
+            return 0.0;
+        }
+        public double RadioLongitud(double numero1)
+        {
+            Console.WriteLine("Ingresar el diametro de la circunferencia: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("El radio de la circunferencia es: " + (numero1 / 2));
+            Console.WriteLine("La longitud de la circunferencia es: " + (Math.PI * numero1));
+            Console.WriteLine("El area de la circunferencia es: " + (Math.PI * (numero1 * 2)));
+            return 0.0;
+
+        }
+
+        public double PromerdioNumeros(double numero1, double numero2, double numero3)
+        {
+
+            Console.WriteLine("Escriba numeros enteros: ");
+            Console.WriteLine("Ingresar el primer numero: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar el segundo numero ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar el tercer numero ");
+            numero3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("El promedio de los 3 numeros ingresados es: " + (numero1 + numero2 + numero3) / 3);
+
+            return 0.0;
+        }
+
+        public double PositivoNegativo(double numero1)
+        {
+            Console.WriteLine("Digite un numero por favor");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            if (numero1 > 0)
+            {
+                Console.WriteLine("el numero que digito es positivo");
+            }
+            else
+            {
+                Console.WriteLine("el numero que digito es negativo");
+            };
+
+            return 0.0;
+        }
+
+        public double MayorMenor(double numero1, double numero2)
+        {
+            Console.WriteLine("Digite el primer numero  por favor");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite el segundo numero  por favor");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("el primer numero " + numero1 + " es el numero mayor    " + "    " + "el segundo numero  " + numero2 + " es el numero menor \n");
+            }
+            else
+            {
+                if (numero1 < numero2)
+                {
+                    Console.WriteLine("el segundo numero  " + numero2 + " es el numero mayor   " + "    " + "el segundo numero  " + numero1 + " es el numero menor       \n ");
+                }
+            };
+
+            return 0.0;
+        }
+
+        public double MenoryMayor(double numero1, double numero2, double numero3)
+        {
+            Console.WriteLine("Ingresa el primer numero");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo numero");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el tercer numero");
+            numero3 = int.Parse(Console.ReadLine());
+
+            if (numero1 < numero2)
+            {
+                if (numero2 < numero3)
+                {
+                    Console.WriteLine("el numero " + numero3 + " es mayor y el numero " + numero1 + " es menor");
+                }
+                else
+                    Console.WriteLine("el numero " + numero2 + " es mayor y el numero " + numero1 + " es menor");
+            }
+            else if (numero1 > numero2)
+            {
+                if (numero1 > numero3)
+                {
+                    Console.WriteLine("el numero " + numero1 + " es mayor y el numero " + numero3 + " es menor");
+                }
+                else
+                    Console.WriteLine("el numero " + numero2 + " es mayor y el numero " + numero3 + " es menor");
+            }
+            Console.ReadKey();
+            return 0.0;
+        }
+
+        public double SumaoResta(double numero1, double numero2)
+        {
+
+
+            Console.WriteLine("digite por favor el numero A");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("digite por favor el numero B");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 < numero2)
+            {
+                Console.WriteLine("La suma entre los numero A y B  es: " + (numero1 + numero2));
+            }
+            else
+            {
+                Console.WriteLine("La resta entre los numero A y B  es: " + (numero1 - numero2));
+            }
+
+            return 0.0;
+        }
+        public double CocienteNumero(double numero1, double numero2)
+        {
+            Console.WriteLine("digite por favor el numero A");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("digite por favor el numero B");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 == 0)
+            {
+                Console.WriteLine("la division por 0 no es posible");
+
+            }
+            else
+            {
+                if (numero2 == 0)
+                {
+                    Console.WriteLine("la division por 0 no es posible");
+                }
+                else
+                {
+                    Console.WriteLine("el cociente entre el numero A y B es " + (numero1 / numero2));
+                }
+            }
+
+            return 0.0;
+        }
+
+        public double NegativoyMultiplicacion(double numero1, double numero2)
+        {
+
+            Console.WriteLine("digite por favor el numero A");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("digite por favor el numero B");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 < 0)
+            {
+                Console.WriteLine("la suma entre A Y B  es : " + (numero1 + numero2));
+
+            }
+            else
+            {
+                if (numero2 < 0)
+                {
+                    Console.WriteLine("la suma entre A Y B  es : " + (numero1 + numero2));
+                }
+                else
+                {
+                    Console.WriteLine("la multiplicacion entre A Y B  es : " + (numero1 * numero2));
+
+                }
+
+            }
+
+            return 0.0;
+        }
+
+        public double AñoBisiesto(double numero1)
+        {
+
+            Console.WriteLine("calcular bisiestos \n");
+            Console.WriteLine("año a calcular \n");
+
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            if (numero1 % 4 == 0 && (numero1) % 100 != 0 || (numero1) % 400 == 0)
+            {
+                Console.WriteLine("Es bisiesto {0}\n", numero1);
+            }
+            else
+            {
+                Console.WriteLine("no es bisiesto {0} \n", numero1);
+            }
+            return 0.0;
+        }
+
+        public double MultiplosDe3(double numero1, double numero2)
+        {
+
+            do
+            {
+                Console.WriteLine("Los múltiplos de tres del cero al cien son:   " + numero1);
+                numero1 = numero1 + 3;
+                numero2 = numero2 + 1;
+            }
+            while (numero1 < 100);
+
+            /*Console.WriteLine("El número de múltiplos de tres encontrados es de:  ", contadorMultiploTres);*/
+            return 0.0;
+        }
+        public double Impares(double numero1)
+        {
+            numero1 = 1;
+            /*int limite = 0;*/
+
+            do
+            {
+                Console.WriteLine("los numeros impares del 1 al 100 son  " + numero1);
+                numero1 = numero1 + 2;
+                /* limite = limite + 1;*/
+
+            } while (numero1 < 100);
+
+            /*Console.WriteLine(" el numero de impares del 1 al 100 es " + limite );*/
+
+            return 0.0;
+        }
+        public double pares(double numero1)
+        {
+            do
+            {
+                Console.WriteLine("los numeros pares del 1 al 100 son  " + numero1);
+                numero1 = numero1 + 2;
+
+
+            } while (numero1 < 100);
+            return 0.0;
+        } 
+        public double Cuadrados30(double numero1, double numero2)
+        {
+
+            /*int limitec = 0;*/
+
+            do
+            {
+                Console.WriteLine("los cuadrados  del 1 al 30 son  " + (numero1) + "*" + (numero1) + " = " + numero2);
+                numero1 = numero1 + 1;
+                numero2 = numero1 * numero1;
+                /*limitec = limitec + 1;*/
+
+            } while (numero1 <= 30);
+            return 0.0;
+        }
+        public double SumaCuadrados(double numero1, double numero2, double numero3)
+        {
+
+            for (double i = numero1; numero1 <= 100; numero1++)
+            {
+
+                /*Console.WriteLine("los cuadrados  del 1 al 100 son  " + (numero1) + "*" + (numero1) + " = " + nu);*/
+
+                numero2 = numero1 * numero1;
+
+
+                numero3 = numero3 + numero2;
+
+            }
+            Console.WriteLine(" la suma de los cuadrados del numero 1 al 100  es  " + numero3);
+
+            return 0.0;
+        }
+        public double NumeroAscendentes(double numero1, double numero2)
+        {
+
+            Console.WriteLine("digite por favor el numero A");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("digite por favor el numero B");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            for (double i = numero1; i < numero2; i++)
+            {
+                while (numero1 == numero2) ;
+                Console.WriteLine("los numero ascendentes hasta llegar a " + numero2 + " son  " + i);
+
+            }
+            return 0.0;
+
+
+        }
+
+        public double SumaTeclado(double numero1, double numero2, double numero3)
+        {
+            do
+            {
+                Console.WriteLine("Introduce un numero: ");
+                numero1 = Convert.ToInt32(Console.ReadLine());
+                if (numero1 != 0)
+                {
+                    numero2 += numero1;
+                    numero3++;
+                }
+            } while (numero1 != 0);
+            if (numero3 == 0)
+            {
+                Console.WriteLine("No se puede realizar la suma");
+            }
+            else
+            {
+                Console.WriteLine("La suma de todos los numeros introducidos es: " + numero2);
+            }
+
+            return 0.0;
+
+        }
+    }
+
+}
+
---
+++ miscelanea.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miscelanea_Diego_Berdugo
+{
+
+    class miscelanea
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            double numero1 = 0.0;
+            double numero2 = 0.0;
+            double numero3 = 0.0;
+
+
+            
+
+            while (true)
+            {
+
+                try
+                {
+
+
+
+                    Console.WriteLine(@"                                        *******MENÚ PRINCIPAL *******
+
+
+
+                                                 1. Operadores.
+
+                                                 2. Condicionales.
+
+                                                 3. Ciclos.
+
+                                                 4. Arreglos.
+
+                                                 0. Salir.
+
+
+
+                                            *******MISCELANEA EN C# *******
+
+                        ");
+
+                    string menu = Console.ReadLine();
+                    Console.Clear();
+
+                    int switch_menu = int.Parse(menu);
+
+                    switch (switch_menu)
+                    {
+                        case 1:
+                            Console.WriteLine(@"                    ******* Operadores *******
+                        
+
+                        1. Calcular el área de un triángulo..
+
+                        2. Suma de dos numeros.
+
+                        3. Elevar un numero al cuadrado.
+
+                        4. Conversor de Euros a dólares.
+
+                        5. Calculador Area y Perimetro de un cuadrado.
+
+                        6. Area y el volúmen de un cilindro
+
+                        7. Realizar un algoritmo que lea el radio de una circunferencia y escriba la longitud de la misma y
+                          el área (pi*r)^2 del círculo inscrito
+
+                        8. Calcular el promedio de tres (3) números
+
+                        0. Salir.
+                        ");
+
+                            string submenu = Console.ReadLine();
+                            Console.Clear();
+                            int switch_submenu = int.Parse(submenu);
+
+                            Ejercicios ejercicios = new Ejercicios();
+
+                            switch (switch_submenu)
+                            {
+
+
+                                case 1:
+
+                                    ejercicios.AreaTriangulo(numero1, numero2);
+
+
+                                    break;
+
+                                case 2:
+
+                                    ejercicios.SumaNumero(numero1, numero2);
+
+                                    break;
+
+                                case 3:
+                                    ejercicios.NumeroCuadrado(numero1, numero2);
+
+                                    break;
+
+                                case 4:
+                                    ejercicios.ConversorEuros(numero1, numero2);
+                                    break;
+
+                                case 5:
+                                    ejercicios.AreaPerimetro(numero1);
+                                    break;
+
+                                case 6:
+                                    ejercicios.AreaVolumen(numero1, numero2);
+                                    break;
+
+                                case 7:
+                                    ejercicios.RadioLongitud(numero1);
+                                    break;
+
+                                case 8:
+                                    ejercicios.PromerdioNumeros(numero1, numero2, numero3);
+                                    break;
+                            }
+                            Console.WriteLine("PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL ");
+
+                            Console.ReadKey();
+                            break;
+
+                        case 2:
+                            Console.WriteLine(@"                    ******* Condicionales ******  
+                           
+
+                           
+                        1. Escribir un algoritmo para saber si el número ingresado por teclado es positivo o negativo..
+
+                        2. Escribir un algoritmo que reciba dos números por teclado y diga cuál es el mayor y cuál el
+                           menor.
+
+                        3. Escriba un programa que lea tres números enteros positivos y que calcule e imprima en
+                           pantalla el menor y el mayor de ellos.
+
+                        4. Da  dos números A y B, sumarlos si A es menor que B o sino restarlos.
+
+                        5. Da  dos números A y B encontrar el cociente entre A y B. Recordar que la división por cero
+                           no está definida, en ese caso debe aparecer una leyenda anunciando que la división no es
+                           posible.
+
+                        6. Da  dos números A y B, sumarlos si al menos uno de ellos es negativo, en caso contrario
+                           multiplicarlos.
+
+                        7. Escribir un algoritmo que determine si un año es bisiesto o no.
+
+                        0. Salir.
+
+                        ");
+
+                            string submenu2 = Console.ReadLine();
+                            Console.Clear();
+
+                            int switch_submenu2 = int.Parse(submenu2);
+
+
+                            Ejercicios condiciones = new Ejercicios();
+
+
+                            switch (switch_submenu2)
+                            {
+                                case 1:
+
+                                    condiciones.PositivoNegativo(numero1);
+
+                                    break;
+
+                                case 2:
+
+                                    condiciones.MayorMenor(numero1, numero2);
+
+                                    break;
+
+                                case 3:
+
+
+                                    condiciones.MenoryMayor(numero1, numero2, numero3);
+                                    break;
+
+                                case 4:
+                                    condiciones.SumaoResta(numero1, numero2);
+                                    break;
+
+                                case 5:
+                                    condiciones.CocienteNumero(numero1, numero2);
+
+                                    break;
+
+                                case 6:
+                                    condiciones.NegativoyMultiplicacion(numero1, numero2);
+
+                                    break;
+
+                                case 7:
+                                    condiciones.AñoBisiesto(numero1);
+                                    break;
+
+
+                            }
+                            Console.WriteLine("PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL ");
+
+                            Console.ReadKey();
+                            break;
+                        case 3:
+                            Console.WriteLine(@"                    ******* Ciclos *******
+
+
+                        1. Imprimir todos los múltiplos de 3 que hay entre 1 y 100.
+
+                        2. Imprimir los números impares entre 0 y 100.
+
+                        3. Imprimir los números pares del 1 al 100.
+
+                        4. Escribir un programa que imprima por pantalla los cuadrados de los números del 1 al 30.
+
+                        5. Escribir un programa que sume los cuadrados de los cien primeros números naturales,
+                        mostrando el resultado en pantalla.
+
+                        6. Dados dos números naturales, el primero menor que el segundo, generar y mostrar todos los
+                        números comprendidos entre ellos en secuencia ascendente.
+
+                        7. Sumar todos los números que se digitan por teclado mientras no sea cero.
+
+                         0. Salir.
+
+                         ");
+
+                            string submenu3 = Console.ReadLine();
+                            Console.Clear();
+
+                            int switch_submenu3 = int.Parse(submenu3);
+
+                            Ejercicios ciclos = new Ejercicios();
+
+                            switch (switch_submenu3)
+                            {
+
+                                case 1:
+
+                                    ciclos.MultiplosDe3(numero1, numero2);
+
+                                    break;
+
+                                case 2:
+                                    ciclos.Impares(numero1);
+
+                                    break;
+
+                                case 3:
+
+                                    ciclos.pares(numero1);
+
+                                    break;
+
+                                case 4:
+                                    ciclos.Cuadrados30(numero1, numero2);
+
+                                    break;
+                                case 5:
+
+                                    ciclos.SumaCuadrados(numero1, numero2, numero3);
+
+                                    break;
+
+                                case 6:
+
+
+                                    ciclos.NumeroAscendentes(numero1, numero2);
+
+                                    break;
+
+                                case 7:
+
+                                    ciclos.SumaTeclado(numero1, numero2, numero3);
+
+
+                                    break;
+                            }
+                            Console.WriteLine("PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL ");
+
+
+
+                            Console.ReadKey();
+                            break;
+
+                        case 4:
+                            Console.WriteLine(@"Arreglos");
+                            Console.ReadKey();
+                            break;
+
+                        case 0:
+                            Console.WriteLine("Gracias");
+                            Console.WriteLine("Codigo realizado por Diego Berdugo ficha 2450473");
+                            Environment.Exit(0);
+                            break;
+                    }
+
+
+                }
+
+                catch (Exception)
+                {
+
+                    Console.WriteLine(" el valor ingresado no es valido , se regresara al menu principal");
+                    Console.ReadLine();
+                    
+                }
+                Console.Clear(); 
+            }
+        }
+    }
+}
